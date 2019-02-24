a765b04a432c41ff12898729956f8c899c0d35fd
Daiju Nakayama <42.daiju@gmail.com>
2019-02-24 17:18:39 +0900

    x,v,t全てにあらわに依存して解析解がある例として減衰+強制振動をテストとした

--- tests/common/harmonic_oscillator.rs
+++ tests/common/harmonic_oscillator.rs
@@ -1,26 +1,85 @@
-use nalgebra::{Vector, Vector2, U2};
+use nalgebra::{Vector, Vector6, U6};
 
 use eom_sim::*;
 
-pub struct Oscillator;
+/// 単振動+減衰+強制振動
+///
+/// 運動方程式は
+///
+/// ```math
+/// a + 2 \zeta \omega_0 v + \omega_0^2 x = f \cos \omega t
+/// ```
+pub struct Oscillator {
+    x0: Vector6<f64>,
+    v0: Vector6<f64>,
+    zeta: Vector6<f64>,
+    omega0: Vector6<f64>,
+    omega: Vector6<f64>,
+    f: Vector6<f64>,
+}
 
 impl Oscillator {
     pub fn new() -> Oscillator {
-        Oscillator {}
+        Oscillator {
+            x0: Vector6::new(1.0, 2.0, 1.5, -1.0, -0.5, 0.0),
+            v0: Vector6::new(0.0, 1.0, -2.0, 0.5, 1.5, 0.0),
+            zeta: Vector6::new(0.0, 0.5, 1.0, 0.8, 1.5, 0.1),
+            omega0: Vector6::new(0.25, 0.5, 1.0, 0.75, 1.25, 0.25),
+            omega: Vector6::new(0.0, 1.0, 0.25, 0.8, 0.5, 0.5),
+            f: Vector6::new(0.0, 0.1, 0.5, 1.0, -1.0, -0.25),
+        }
     }
 
-    pub fn exact_state(&self, t: f64) -> (Vector2<f64>, Vector2<f64>) {
-        (
-            Vector2::new(2.0 * t.cos(), 3.0 * (t * 0.5).sin()),
-            Vector2::new(-2.0 * t.sin(), 1.5 * (t * 0.5).cos()),
-        )
+    pub fn init_state(&self) -> (Vector6<f64>, Vector6<f64>) {
+        (self.x0.clone(), self.v0.clone())
     }
+
+    pub fn exact_position(&self, t: f64) -> Vector6<f64> {
+        Vector6::from_fn(|i, _| {
+            let z = self.zeta[i];
+            let w0 = self.omega0[i];
+            let w = self.omega[i];
+            let f = self.f[i];
+            let (px0, pv0) = particular_solution(0.0, z, w0, w, f);
+            let g = general_solution(t, self.x0[i] - px0, self.v0[i] - pv0, z, w0);
+            let p = particular_solution(t, z, w0, w, f).0;
+            g + p
+        })
+    }
+}
+
+fn general_solution(t: f64, x0: f64, v0: f64, zeta: f64, omega0: f64) -> f64 {
+    assert!(zeta >= 0.0);
+    if zeta == 0.0 {
+        let t = omega0 * t;
+        x0 * t.cos() + v0 / omega0 * t.sin()
+    } else if zeta < 1.0 {
+        let tt = omega0 * (1.0 - zeta * zeta).sqrt();
+        let a = (v0 + zeta * omega0 * x0) / tt;
+        (-zeta * omega0 * t).exp() * (x0 * (tt * t).cos() + a * (tt * t).sin())
+    } else if zeta == 1.0 {
+        (-omega0 * t).exp() * (x0 + (v0 + omega0 * x0) * t)
+    } else {
+        let tt = omega0 * (zeta * zeta - 1.0).sqrt();
+        let a = (v0 + zeta * omega0 * x0) / tt;
+        (-zeta * omega0 * t).exp() * (x0 * (tt * t).cosh() + a * (tt * t).sinh())
+    }
+}
+
+fn particular_solution(t: f64, zeta: f64, omega0: f64, omega: f64, f: f64) -> (f64, f64) {
+    let t = omega * t;
+    let p = 2.0 * zeta * omega0 * omega;
+    let q = omega0 * omega0 - omega * omega;
+    (
+        f * (p * t.sin() + q * t.cos()) / (p * p + q * q),
+        f * omega * (p * t.cos() - q * t.sin()) / (p * p + q * q),
+    )
 }
 
 impl ModelSpec for Oscillator {
     type Time = f64;
     type Scalar = f64;
-    type Dim = U2;
+    type Dim = U6;
 }
 
 impl<S> Explicit<S> for Oscillator
@@ -29,12 +88,19 @@ where
 {
     fn acceleration(
         &mut self,
-        _t: Self::Time,
+        t: Self::Time,
         x: &Vector<Self::Scalar, Self::Dim, S>,
-        _v: &Vector<Self::Scalar, Self::Dim, S>,
+        v: &Vector<Self::Scalar, Self::Dim, S>,
         a: &mut Vector<Self::Scalar, Self::Dim, S>,
     ) {
-        a[0] = -x[0];
-        a[1] = -0.25 * x[1];
+        a.iter_mut()
+            .zip(0..6)
+            .map(|(a, i)| {
+                let p = -2.0 * self.zeta[i] * self.omega0[i] * v[i];
+                let q = -self.omega0[i] * self.omega0[i] * x[i];
+                let r = self.f[i] * (self.omega[i] * t).cos();
+                *a = p + q + r;
+            })
+            .last();
     }
 }
--- tests/runge_kutta.rs
+++ tests/runge_kutta.rs
@@ -2,7 +2,7 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 use eom_sim::*;
-use nalgebra::Vector2;
+use nalgebra::Vector6;
 
 mod common;
 use common::harmonic_oscillator::Oscillator;
@@ -10,69 +10,80 @@ use common::harmonic_oscillator::Oscillator;
 #[test]
 fn oscillator_rk1() {
     let eom = Rc::new(RefCell::new(Oscillator::new()));
-    let mut rk = RK1::new(eom.clone(), Vector2::zeros());
-    let mut err = Vec::new();
+    let mut rk = RK1::new(eom.clone(), Vector6::zeros());
+    let mut err = vec![vec![], vec![], vec![]];
     for i in 8..16 {
         let mut t = 0.0;
-        let (mut x, mut v) = eom.borrow().exact_state(0.0);
+        let (mut x, mut v) = eom.borrow().init_state();
         let dt = 2.0_f64.powi(-i);
-        for _ in 0..1 << i {
-            rk.iterate(&mut t, &mut x, &mut v, dt);
+        for j in 0..err.len() {
+            for _ in 0..1 << i {
+                rk.iterate(&mut t, &mut x, &mut v, dt);
+            }
+            assert_eq!(t, (j + 1) as f64);
+            let ex = eom.borrow().exact_position(t);
+            err[j].push((x - ex).norm());
         }
-        assert_eq!(t, 1.0);
-        let (ex, ev) = eom.borrow().exact_state(t);
-        err.push((x - ex).norm() + (v - ev).norm());
     }
-    let sig = 1.01;
-    for i in 0..err.len() - 1 {
-        let rate = err[i] / err[i + 1];
-        assert!(2.0 / sig < rate && rate < 2.0 * sig);
+    for e in err {
+        for i in 0..e.len() - 1 {
+            let rate = e[i] / e[i + 1];
+            println!("{} {} {}", e[i], e[i + 1], rate);
+            assert!(2.0 * 0.9 < rate && rate < 4.0);
+        }
     }
 }
 
 #[test]
 fn oscillator_rk2() {
     let eom = Rc::new(RefCell::new(Oscillator::new()));
-    let mut rk = RK2::new(eom.clone(), Vector2::zeros());
-    let mut err = Vec::new();
-    for i in 8..16 {
+    let mut rk = RK2::new(eom.clone(), Vector6::zeros());
+    let mut err = vec![vec![], vec![], vec![]];
+    for i in 6..14 {
         let mut t = 0.0;
-        let (mut x, mut v) = eom.borrow().exact_state(0.0);
+        let (mut x, mut v) = eom.borrow().init_state();
         let dt = 2.0_f64.powi(-i);
-        for _ in 0..1 << i {
-            rk.iterate(&mut t, &mut x, &mut v, dt);
+        for j in 0..err.len() {
+            for _ in 0..1 << i {
+                rk.iterate(&mut t, &mut x, &mut v, dt);
+            }
+            assert_eq!(t, (j + 1) as f64);
+            let ex = eom.borrow().exact_position(t);
+            err[j].push((x - ex).norm());
         }
-        assert_eq!(t, 1.0);
-        let (ex, ev) = eom.borrow().exact_state(t);
-        err.push((x - ex).norm() + (v - ev).norm());
     }
-    let sig = 1.01;
-    for i in 0..err.len() - 1 {
-        let rate = err[i] / err[i + 1];
-        assert!(4.0 / sig < rate && rate < 4.0 * sig);
+    for e in err {
+        for i in 0..e.len() - 1 {
+            let rate = e[i] / e[i + 1];
+            println!("{} {} {}", e[i], e[i + 1], rate);
+            assert!(4.0 * 0.9 < rate && rate < 8.0);
+        }
     }
 }
 
 #[test]
 fn oscillator_rk4() {
     let eom = Rc::new(RefCell::new(Oscillator::new()));
-    let mut rk = RK4::new(eom.clone(), Vector2::zeros());
-    let mut err = Vec::new();
+    let mut rk = RK4::new(eom.clone(), Vector6::zeros());
+    let mut err = vec![vec![], vec![], vec![]];
     for i in 2..10 {
         let mut t = 0.0;
-        let (mut x, mut v) = eom.borrow().exact_state(0.0);
+        let (mut x, mut v) = eom.borrow().init_state();
         let dt = 2.0_f64.powi(-i);
-        for _ in 0..1 << i {
-            rk.iterate(&mut t, &mut x, &mut v, dt);
+        for j in 0..err.len() {
+            for _ in 0..1 << i {
+                rk.iterate(&mut t, &mut x, &mut v, dt);
+            }
+            assert_eq!(t, (j + 1) as f64);
+            let ex = eom.borrow().exact_position(t);
+            err[j].push((x - ex).norm());
         }
-        assert_eq!(t, 1.0);
-        let (ex, ev) = eom.borrow().exact_state(t);
-        err.push((x - ex).norm() + (v - ev).norm());
     }
-    let sig = 1.01;
-    for i in 0..err.len() - 1 {
-        let rate = err[i] / err[i + 1];
-        println!("{} {} {}", err[i], err[i + 1], rate);
-        assert!(16.0 / sig < rate && rate < 16.0 * sig);
+    for e in err {
+        for i in 0..e.len() - 1 {
+            let rate = e[i] / e[i + 1];
+            println!("{} {} {}", e[i], e[i + 1], rate);
+            assert!(16.0 * 0.9 < rate && rate < 32.0);
+        }
     }
 }
