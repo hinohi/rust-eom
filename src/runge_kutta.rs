f471190c8616cc097a0ebe857318c9cd3adb53d8
Nakayama Daiju <42.daiju@gmail.com>
2022-02-11 21:36:17 +0900

    RK4のアロケーション削減

--- src/runge_kutta.rs
+++ src/runge_kutta.rs
@@ -145,34 +145,34 @@ where
         let six = two + two + two;
         let dt2 = dt / two;
         let dt6 = dt / six;
+        let mut x_tmp = vec![Self::Scalar::zero(); n];
+        let mut v_tmp = vec![Self::Scalar::zero(); n];
         // k1
-        let mut x1 = vec![Self::Scalar::zero(); n];
-        let mut v1 = vec![Self::Scalar::zero(); n];
         let mut a1 = vec![Self::Scalar::zero(); n];
         self.eom.acceleration(*t, x, v, &mut a1);
-        zip_apply!(xx in x1.iter_mut(), &x in x.iter(), &v in v.iter(); *xx = x + v * dt2);
-        zip_apply!(vv in v1.iter_mut(), &v in v.iter(), &a in a1.iter(); *vv = v + a * dt2);
+        zip_apply!(xx in x_tmp.iter_mut(), &x in x.iter(), &v in v.iter(); *xx = x + v * dt2);
+        zip_apply!(vv in v_tmp.iter_mut(), &v in v.iter(), &a in a1.iter(); *vv = v + a * dt2);
         // k2
-        let mut x2 = vec![Self::Scalar::zero(); n];
-        let mut v2 = vec![Self::Scalar::zero(); n];
+        let mut v23 = vec![Self::Scalar::zero(); n];
         let mut a2 = vec![Self::Scalar::zero(); n];
-        self.eom.acceleration(*t + dt2, &x1, &v1, &mut a2);
-        zip_apply!(xx in x2.iter_mut(), &x in x.iter(), &v in v1.iter(); *xx = x + v * dt2);
-        zip_apply!(vv in v2.iter_mut(), &v in v.iter(), &a in a2.iter(); *vv = v + a * dt2);
+        self.eom.acceleration(*t + dt2, &x_tmp, &v_tmp, &mut a2);
+        zip_apply!(xx in x_tmp.iter_mut(), &x in x.iter(), &v in v_tmp.iter(); *xx = x + v * dt2);
+        zip_apply!(vv in v23.iter_mut(), &v in v.iter(), &a in a2.iter(); *vv = v + a * dt2);
         // k3
-        let mut x3 = vec![Self::Scalar::zero(); n];
-        let mut v3 = vec![Self::Scalar::zero(); n];
         let mut a3 = vec![Self::Scalar::zero(); n];
-        self.eom.acceleration(*t + dt2, &x2, &v2, &mut a3);
-        zip_apply!(xx in x3.iter_mut(), &x in x.iter(), &v in v2.iter(); *xx = x + v * dt);
-        zip_apply!(vv in v3.iter_mut(), &v in v.iter(), &a in a3.iter(); *vv = v + a * dt);
+        self.eom.acceleration(*t + dt2, &x_tmp, &v23, &mut a3);
+        zip_apply!(xx in x_tmp.iter_mut(), &x in x.iter(), &v in v23.iter(), vv in v_tmp.iter_mut(); {
+            *xx = x + v * dt;
+            *vv += v;
+        });
+        zip_apply!(vv in v23.iter_mut(), &v in v.iter(), &a in a3.iter(); *vv = v + a * dt);
         // k4
         let mut a4 = vec![Self::Scalar::zero(); n];
-        self.eom.acceleration(*t + dt, &x3, &v3, &mut a4);
+        self.eom.acceleration(*t + dt, &x_tmp, &v23, &mut a4);
         // sum
         zip_apply!(
-            x in x.iter_mut(), &v in v.iter(), &v1 in v1.iter(), &v2 in v2.iter(), &v3 in v3.iter();
-            *x += (v + (v1 + v2) * two + v3) * dt6
+            x in x.iter_mut(), &v in v.iter(), &v12 in v_tmp.iter(), &v3 in v23.iter();
+            *x += (v + v12 * two + v3) * dt6
         );
         zip_apply!(
             v in v.iter_mut(), &a1 in a1.iter(), &a2 in a2.iter(), &a3 in a3.iter(), &a4 in a4.iter();
